6f3199b09b1f442a4fd65d47d3dde49a9f16ff2b
Ivan Gonzalez <igonzalez@grupoexcel.com>
2023-07-05 13:55:02 -0700

    feat(frontend): Fetch Data By React Query

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,15 +1,15 @@
-import { Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { sampleProducts } from "../data";
-import { Product } from "../types/Product";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { useEffect, useReducer } from "react";
-import axios from "axios";
-import { getError } from "../utils";
-import { ApiError } from "../types/ApiError";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import ProductItem from "../components/ProductItem";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios";
+import { ApiError } from "../types/ApiError";
 import { Helmet } from "react-helmet-async";
+import { Product } from "../types/Product";
+import { getError } from "../utils";
+import { useGetProductsQuery } from "../hooks/productHooks";
 
 type State = {
   products: Product[];
@@ -44,33 +44,18 @@ const reducer = (state: State, action: Action) => {
 };
 
 export default function HomePage() {
-  const [{ loading, error, products }, dispatch] = useReducer<
-    React.Reducer<State, Action>
-  >(reducer, initialState);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      dispatch({ type: "FETCH_REQUEST" });
-      try {
-        const result = await axios.get("/api/products");
-        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
-      } catch (error) {
-        dispatch({ type: "FETCH_FAIL", payload: getError(error as ApiError) });
-      }
-    };
-    fetchData();
-  }, []);
-
-  return loading ? (
+  return isLoading ? (
     <LoadingBox />
   ) : error ? (
-    <MessageBox variant="danger">{error}</MessageBox>
+    <MessageBox variant="danger">{getError(error as ApiError)}</MessageBox>
   ) : (
     <Row>
       <Helmet>
         <title>TS Amazona</title>
       </Helmet>
-      {products.map((product) => (
+      {products!.map((product) => (
         <Col key={product.slug} sm={6} md={4} lg={3}>
           <ProductItem product={product} />
         </Col>
